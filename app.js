ca461351502cb4168fd6b372565e4fe8e2086e44
iAmAnkurit <bankurit1891@gmail.com>
2024-11-03 21:11:39 +0530

    Added login, authentication

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express");
 const app = express();
 
 const userModel = require("./models/user");
+const postModel = require("./models/post");
+
 const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 app.set("view engine", "ejs");
 app.use(express.json());
@@ -23,6 +26,51 @@ app.post("/register", async (req, res) => {
   if (user) {
     return res.status(500).send("User already registered");
   }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, async (err, hash) => {
+      let user = userModel.create({
+        name,
+        username,
+        email,
+        age,
+        password: hash,
+      });
+
+      let token = jwt.sign({ email: email, userId: user._id }, "SecrectKey");
+      res.cookie("token", token);
+      res.send("User is register");
+    });
+  });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  let { email, password } = req.body;
+
+  let user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(500).send("User does not exist");
+  }
+
+  bcrypt.compare(password, user.password, (err, result) => {
+    if (result) {
+      let token = jwt.sign({ email: email, userId: user._id }, "SecrectKey");
+      res.cookie("token", token);
+      res.status(200).send(`Hi, ${user.username}`);
+    } else {
+      res.status(422).send("Email or password is incorrect.");
+    }
+  });
+});
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/login");
 });
 
 app.listen(3000);
